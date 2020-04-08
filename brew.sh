846555058e8d16f431d629cac5aeaf4ea3968890
veit <kontakt@veit-schiele.de>
2020-04-08 17:57:14 +0200

    Remove options that are no longer used

--- brew.sh
+++ brew.sh
@@ -22,7 +22,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed 
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
