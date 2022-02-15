2c279881ec1673093f37c97b0106f9e1012075ff
veit <kontakt@veit-schiele.de>
2022-02-15 17:49:36 +0100

    Install and configure spack

--- install.sh
+++ install.sh
@@ -2,8 +2,20 @@
 DOTFILES="$(dirname $(realpath $0))"
 [ -L ~/.config ] || ln -s $DOTFILES/.config ~/.config
 grep -q DOTFILES ~/.bash_profile || echo "export DOTFILES=$DOTFILES" >> ~/.bash_profile
+# Install spack
+xcode-select --install
+brew install make bash gzip bzip2 git gnupg
+brew link gnupg
+if [[ ! -d ~/spack ]]
+then
+    cd && git clone https://github.com/spack/spack.git
+fi
+cd ~/spack
+git checkout releases/v0.17
+
 python3 -m pip install --user pipenv
-for script in brew pipenv nbstrip;
+
+for script in brew spack pipenv nbstrip;
 do
 grep -q $script ~/.bash_profile || echo "source $DOTFILES/$script.sh" >> ~/.bash_profile
 done
--- spack.sh
+++ spack.sh
@@ -1,3 +1,4 @@
+# Spack
 # The file is part of Spack and sets up the spack environment for bash
 export SPACK_ROOT=~/spack
 . $SPACK_ROOT/share/spack/setup-env.sh
