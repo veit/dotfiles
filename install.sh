fe553d7c0c1e7d97a594b85b67350cc51b464f8a
veit <kontakt@veit-schiele.de>
2022-02-15 14:44:39 +0100

    Add DOTFILES variable to bash profile

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DOTFILES="$(dirname $(realpath $0))"
 [ -L ~/.config ] || ln -s $DOTFILES/.config ~/.config
+grep -q DOTFILES ~/.bash_profile || echo "export DOTFILES=$DOTFILES" >> ~/.bash_profile
 python3 -m pip install --user pipenv
