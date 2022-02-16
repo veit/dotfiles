7507f37dfd7ee7e57e8c1be205bb62e00863882d
veit <kontakt@veit-schiele.de>
2022-02-16 08:56:18 +0100

    Update command line tools

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 DOTFILES="$(dirname $(realpath $0))"
 [ -L ~/.config ] || ln -s $DOTFILES/.config ~/.config
 grep -q DOTFILES ~/.bash_profile || echo "export DOTFILES=$DOTFILES" >> ~/.bash_profile
+
 # Install spack
 xcode-select --install
+# Update Command Line Tools
+softwareupdate --all --install --force
 brew install make bash gzip bzip2 git gnupg
 brew link gnupg
 if [[ ! -d ~/spack ]]
